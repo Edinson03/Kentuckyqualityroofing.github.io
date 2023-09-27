2b4e99d4e68f0aba6cdc4beb0842757f9b178f5f
Edinson03 <edinson.rodriguez@utp.edu.co>
2023-09-27 16:07:23 -0500

    Avance medio

--- main.js
+++ main.js
@@ -6,8 +6,26 @@ let typewriter = new Typewriter(app, {
 });
  
 typewriter
-  .pauseFor(2500)
+  .pauseFor(2500) 
   .typeString('La Capital del Sol')
   .pauseFor(200)
   .deleteChars(10)
   .start();
+
+// Obtén el botón de cambio de idioma por su id
+const toggleLanguageButton = document.getElementById('toggle-language-button');
+
+// Agrega un event listener al botón
+toggleLanguageButton.addEventListener('click', function () {
+    // Cambia el idioma a español
+    document.querySelector('html').setAttribute('lang', 'es');
+});
+
+// Función para cambiar el idioma
+function changeLanguage() {
+    // Cambia el idioma a español
+    document.querySelector('html').setAttribute('lang', 'es');
+}
+
+
+  
