fdf34c01f77394d52dd2604f71810b9125059561
Edinson03 <edinson.rodriguez@utp.edu.co>
2023-09-29 00:24:05 -0500

    Boton funcional de cambio de idioma y detalles de iamgenes

--- main.js
+++ main.js
@@ -1,31 +1,80 @@
-let app = document.getElementById('typewriter');
- 
-let typewriter = new Typewriter(app, {
-  loop: true,
-  delay: 75,
-});
- 
-typewriter
-  .pauseFor(2500) 
-  .typeString('La Capital del Sol')
-  .pauseFor(200)
-  .deleteChars(10)
-  .start();
+// Variables globales para almacenar las traducciones
+let translations = {};
 
-// Obtén el botón de cambio de idioma por su id
-const toggleLanguageButton = document.getElementById('toggle-language-button');
-
-// Agrega un event listener al botón
-toggleLanguageButton.addEventListener('click', function () {
-    // Cambia el idioma a español
-    document.querySelector('html').setAttribute('lang', 'es');
-});
-
-// Función para cambiar el idioma
-function changeLanguage() {
-    // Cambia el idioma a español
-    document.querySelector('html').setAttribute('lang', 'es');
+// Función para cargar las traducciones desde un archivo JSON
+function loadTranslations(language) {
+  fetch(`${language}.json`)
+    .then((response) => response.json())
+    .then((data) => {
+      translations = data;
+      // Aplicar las traducciones
+      changeLanguage(language);
+    })
+    .catch((error) => {
+      console.error(`Error loading translations for ${language}:`, error);
+    });
 }
 
+// Función para cambiar el idioma
+function changeLanguage(language) {
+  // Obtener todos los elementos que deseas traducir
+  const elementsToTranslate = document.querySelectorAll("[data-translate]");
 
-  
+  // Iterar sobre los elementos y aplicar la traducción
+  elementsToTranslate.forEach((element) => {
+    const translationKey = element.getAttribute("data-translate");
+    element.textContent = translations[translationKey];
+  });
+}
+
+// Inicializar el idioma en español
+let currentLanguage = "es";
+
+// Llamar a la función para cargar las traducciones iniciales
+loadTranslations(currentLanguage);
+
+// Manejar el evento de cambio de idioma (por ejemplo, cuando se hace clic en el botón)
+const toggleLanguageButton = document.getElementById("toggle-language-button");
+toggleLanguageButton.addEventListener("click", () => {
+  // Cambiar entre español (es) e inglés (en)
+  if (currentLanguage === "es") {
+    currentLanguage = "en";
+  } else {
+    currentLanguage = "es";
+  }
+
+  // Cargar las traducciones actualizadas
+  loadTranslations(currentLanguage);
+});
+
+
+const video = document.getElementById("myVideo");
+let isPlaying = false;
+
+function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function checkVideoPlay() {
+    if (isElementInViewport(video) && !isPlaying) {
+        video.play().catch(error => {
+            // Manejar errores, si es necesario
+        });
+        isPlaying = true;
+    } else if (!isElementInViewport(video) && isPlaying) {
+        video.pause();
+        isPlaying = false;
+    }
+}
+
+// Agregar un event listener para verificar cuando se haga scroll
+window.addEventListener("scroll", checkVideoPlay);
+
+// Verificar el estado del video cuando la página se carga
+window.addEventListener("load", checkVideoPlay);
